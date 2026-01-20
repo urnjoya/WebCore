451b8b8fcbb1eb5cc3892352d20e5dcdd1407a5f
Joya <urnjoya@gmail.com>
2026-01-21 01:03:44 +0530

    Implement caching and versioning in service worker
    Added service worker caching logic and versioning.

--- sw.js
+++ sw.js
@@ -14,4 +14,24 @@ self.addEventListener("notificationclick", event => {
       clients.openWindow("/")
     );
   }
+
+});
+// ------------------ Service Worker for PWA ------------------
+const APP_VERSION = "1.0.2";
+const CACHE_NAME = `oneweb-${APP_VERSION}`;
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+  );
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k))
+      )
+    )
+  );
 });
